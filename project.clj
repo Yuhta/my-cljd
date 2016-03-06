d3974b5e55abda813453bbcc3450e569d9351b09
Jimmy Lu <gongchuo.lu@gmail.com>
2016-03-06 18:43:40 -0500

    Add gcal2org

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.martiansoftware/nailgun-server "0.9.1"]
                  [mwdict "0.1.0-SNAPSHOT"]
-                 [yuhta/check-gmail "0.1.0-SNAPSHOT"]]
+                 [yuhta/check-gmail "0.1.0-SNAPSHOT"]
+                 [yuhta/gcal2org "0.1.0-SNAPSHOT"]]
   :plugins [[lein-bin "0.3.5"]]
   :main com.martiansoftware.nailgun.NGServer
   :target-path "target/%s"
